08bcb0a7c5d6a40d0516f0e31500874acf791c0f
required-true <lionjk92@gmail.com>
2023-06-13 22:30:50 +0900

    Recoil-persist

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,4 +1,5 @@
 import { atom, selector } from 'recoil'
+import { recoilPersist } from 'recoil-persist'
 
 export enum Categories {
   'TO_DO' = 'TO_DO',
@@ -12,14 +13,26 @@ export interface ITodo {
   text: string
 }
 
+const { persistAtom: categoryPersist } = recoilPersist({
+  key: 'CATEGORY',
+  storage: localStorage,
+})
+
+const { persistAtom: todoPersist } = recoilPersist({
+  key: 'TODO',
+  storage: localStorage,
+})
+
 export const categoryState = atom<Categories>({
   key: 'category',
   default: Categories.TO_DO,
+  effects_UNSTABLE: [categoryPersist],
 })
 
 export const todoState = atom<ITodo[]>({
   key: 'todo',
   default: [],
+  effects_UNSTABLE: [todoPersist],
 })
 
 export const todoSelector = selector({
